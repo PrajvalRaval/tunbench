f253e6672a8a4515378f756fab4f6ceea04c046f
Prajval Raval <ravalprajval@gmail.com>
2024-01-08 20:20:26 +0530

    fix setup

--- setup.sh
+++ setup.sh
@@ -10,15 +10,18 @@ sudo ip addr add 192.0.3.1 peer 192.0.3.2 dev tun1
 
 sudo iptables -A FORWARD -i tun0 -s 192.0.2.2 -j ACCEPT
 sudo iptables -A FORWARD -i tun1 -s 192.0.3.2 -j ACCEPT
+
 sudo iptables -A FORWARD -o tun0 -d 192.0.2.2 -j ACCEPT
 sudo iptables -A FORWARD -o tun1 -d 192.0.3.2 -j ACCEPT
 
-sudo iptables -A INPUT -i tun1 -m state --state RELATED,ESTABLISHED -j ACCEPT
-sudo iptables -A INPUT -i tun0 -m state --state RELATED,ESTABLISHED -j ACCEPT
-
 sudo sysctl -w net.ipv4.ip_forward=1
-sudo sysctl net.ipv4.conf.tun0.accept_local=1
-sudo sysctl net.ipv4.conf.tun1.accept_local=1
-sudo sysctl -p
 
-sudo ip route flush cache
+
+# sudo iptables -A INPUT -i tun1 -m state --state RELATED,ESTABLISHED -j ACCEPT
+# sudo iptables -A INPUT -i tun0 -m state --state RELATED,ESTABLISHED -j ACCEPT
+
+# sudo sysctl net.ipv4.conf.tun0.accept_local=1
+# sudo sysctl net.ipv4.conf.tun1.accept_local=1
+# sudo sysctl -p
+
+# sudo ip route flush cache
