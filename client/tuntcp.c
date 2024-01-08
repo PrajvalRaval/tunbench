f39b50e308494dede023435823edf161f99f07fc
Prajval Raval <ravalprajval@gmail.com>
2024-01-09 00:37:38 +0530

    arr*

--- client/tuntcp.c
+++ client/tuntcp.c
@@ -77,7 +77,7 @@ void TCPConnection(int tun, char *addr, uint16_t port, struct tcp_conn *conn)
 
 void send_tcp_packet(struct tcp_conn *conn, uint8_t flags)
 {
-	char data[1024];
+	char *data[1024];
 
 	struct tcp tcp;
 	TCP(conn->src_port, conn->dst_port, conn->seq, conn->ack, flags, &tcp);
